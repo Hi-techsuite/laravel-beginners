88f2c2181ca47f9a4acd5be8d2f93f2c7704ce71
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-08-17 01:03:29 +0100

    first entry

---
+++ app/Http/Controllers/GoodController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GoodController extends Controller
+{
+    //
+
+
+    public function mainMethod(){
+        return view('welcome'); 
+    }
+}
